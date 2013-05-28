3a6892fdc3f43572ceb97144b2f1ca383b9508b7
Aaron Sikes <jingadjanga@gmail.com>
2013-05-28 18:38:21 -0400

    Fetched all the keys, and can filter to entry points or reference spots

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+var async = require('async');
 var redis = require('redis');
+
+var guid = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
+
+var client = redis.createClient();
+
+client.keys('*', function(error, keys){
+	if (error) return err(error);
+});
+
+
+function get_entry_points(keys){
+	return keys.filter(function(item){
+		return !guid.test(item);
+	});
+}
+
+function get_hashes(keys){
+	return keys.filter(function(item){
+		return guid.test(item);
+	})
+}
+
+function err(error){
+	console.error(error);
+}
